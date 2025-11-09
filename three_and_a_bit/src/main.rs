0d560b60ccdf43ff306dbdbd0abc89f459add30b
Siva Karasala <shivataraya@gmail.com>
2025-11-09 18:34:34 +0530

    feat: three-and-a-bit

---
+++ three_and_a_bit/src/main.rs
@@ -0,0 +1,6 @@
+fn main() {
+    const THREE_AND_A_BIT: f32 = 3.4028236;
+    println!("{}", THREE_AND_A_BIT);
+    const THREE_AND_A_BIT_V1: f64 = 3.4028236;
+     println!("{}", THREE_AND_A_BIT_V1);
+}
