f7715b944471ab866725588eb3fbfd7edaa387c7
Siva Karasala <shivataraya@gmail.com>
2025-11-15 19:31:38 +0530

    feat: add const_loop

---
+++ const_loop/src/main.rs
@@ -0,0 +1,23 @@
+const fn fib(n: u128) -> u128 {
+    let mut a = 1;
+    let mut b = 1;
+    // for _ in 2..n {
+    //     let tmp = a + b;
+    //     a = b;
+    //     b = tmp;
+    // }
+    let mut counter = 2;
+    while counter < n {
+        let tmp = a + b;
+        a = b;
+        b = tmp;
+        counter += 1;
+    }
+    b
+}
+
+fn main() {
+   for i in 0..5 {
+    println!("Fib {} = {}", i, fib(i));
+   }
+}
