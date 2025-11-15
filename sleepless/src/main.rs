283f2b9dd61f943d2a24bf6ccb869baba38e8774
Siva Karasala <shivataraya@gmail.com>
2025-11-15 18:47:15 +0530

    feat: add sleepless tokio

---
+++ sleepless/src/main.rs
@@ -0,0 +1,14 @@
+use tokio::join;
+use std::time::Duration;
+
+async fn count_and_wait(n: u64) -> u64 {
+    println!("Starting {}", n);
+    std::thread::sleep(Duration::from_millis(n * 100));
+    println!("Returning {}", n);
+    n
+}
+#[tokio::main]
+async fn main() -> Result<(), Box<dyn std::error::Error>> {
+    join!(count_and_wait(1), count_and_wait(2), count_and_wait(3));
+    Ok(())
+}
