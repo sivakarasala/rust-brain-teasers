e3ef60a751ab82144b37e07e4dd474aa1ed63ee5
Siva Karasala <shivataraya@gmail.com>
2025-11-15 19:21:03 +0530

    feat: add gordian_knot

---
+++ gordian_knot/src/main.rs
@@ -0,0 +1,23 @@
+#[derive(Debug)]
+struct Parser<'a> {
+    body: String,
+    subtext: &'a str,
+}
+
+fn main() {
+    // let mut document = Parser {
+    //     body: "Hello".to_string(),
+    //     subtext: "",
+    // };
+    // document.subtext = &document.body;
+
+    // let b = document;
+    // println!("{:?}", b);
+    {
+        let mut document = Parser {
+            body: "Hello".to_string(),
+            subtext: "",
+        };
+        document.subtext = &document.body;
+    }
+}
