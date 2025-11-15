988a6e8b2e47d55d6bc1a6274c727847b0f5b4fb
Siva Karasala <shivataraya@gmail.com>
2025-11-15 11:51:11 +0530

    feat: add type conversion

---
+++ type_conversion/src/main.rs
@@ -0,0 +1,13 @@
+use std::u32;
+
+fn main() {
+    let x: u64 = 4_294_967_296;
+    let y = x as u32;
+    if x == y as u64 {
+        println!("x equals y.");
+    } else {
+        println!("x does not equal y.");
+    }
+    let y = u32::MAX;
+    let z: u64 = y.into();
+}
