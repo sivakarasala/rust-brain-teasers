0fd6295a334714701226448caec15b77dd723470
Siva Karasala <shivataraya@gmail.com>
2025-11-15 11:47:46 +0530

    feat: add std input

--- three_and_a_bit/src/main.rs
+++ three_and_a_bit/src/main.rs
@@ -2,5 +2,5 @@ fn main() {
     const THREE_AND_A_BIT: f32 = 3.4028236;
     println!("{}", THREE_AND_A_BIT);
     const THREE_AND_A_BIT_V1: f64 = 3.4028236;
-     println!("{}", THREE_AND_A_BIT_V1);
+    println!("{}", THREE_AND_A_BIT_V1);
 }
